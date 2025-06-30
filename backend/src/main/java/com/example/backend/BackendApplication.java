410b600339b0f35bf39ac8ca35f64ae95e6577fc
vivekcommit <vivek.singh89@live.com>
2025-06-30 11:37:51 -0500

    Deleted data.sql references. Suppressed null warning for WebMvcConfigurer.

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -16,6 +16,7 @@ public class BackendApplication {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
+			@SuppressWarnings("null")
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
